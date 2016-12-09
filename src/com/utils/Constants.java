a611d8ca3396aa83953a2fefafa402fde945a5d1
thelittlefireman <thomas.goureau@gmail.com>
2016-12-09 16:44:14 +0100

    fix compile error

--- src/com/utils/Constants.java
+++ src/com/utils/Constants.java
@@ -4,7 +4,7 @@ package com.utils;
  * Created by thoma on 09/12/2016.
  */
 public class Constants {
-    public static final boolean DEBUG = false;
+    public static boolean DEBUG = false;
     public static final int NB_CAPTEUR = 8;
     public static final int CORE_POOL_SIZE = 20;
     public static void showDebug(String value){
