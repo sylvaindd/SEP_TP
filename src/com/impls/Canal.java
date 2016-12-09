427067ee6ee3136f1271b0eae9233740136da15c
sylvaindd <sylvainduarte@hotmail.fr>
2016-12-09 17:40:50 +0100

    Random

--- src/com/impls/Canal.java
+++ src/com/impls/Canal.java
@@ -7,10 +7,8 @@ import com.interfaces.CapteurAsync;
 import com.interfaces.ObserveurDeCapteur;
 import com.utils.Constants;
 
-import java.util.concurrent.Future;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.Random;
+import java.util.concurrent.*;
 
 /**
  * Created by Sylvain on 28/11/2016.
@@ -36,14 +34,16 @@ public class Canal implements ObserveurDeCapteur, CapteurAsync {
     @Override
     public Future getValue() {
         GetValue value = new GetValue(mCapteur, mId);
-        return mScheduler.schedule(value, 810, TimeUnit.MILLISECONDS);
+        int random = ThreadLocalRandom.current().nextInt(500, 2000 + 1);
+        return mScheduler.schedule(value, random, TimeUnit.MILLISECONDS);
     }
 
     @Override
     public Future update(Capteur s) {
         mCapteur = s;
         Update update = new Update(this, mAfficheur);
-        return mScheduler.schedule(update, 720, TimeUnit.MILLISECONDS);
+        int random = ThreadLocalRandom.current().nextInt(500, 2000 + 1);
+        return mScheduler.schedule(update, random, TimeUnit.MILLISECONDS);
     }
 
     @Override
